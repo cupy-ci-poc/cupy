8457bf9e272183dabe38a301ba7ff75f84ce1b03
Daniel Rodriguez <daniel@danielfrg.com>
2024-11-08 14:51:10 -0600

    ignore stuff

--- .pfnci/linux/update-cuda-driver.sh
+++ .pfnci/linux/update-cuda-driver.sh
@@ -11,7 +11,7 @@ else
 fi
 
 killall Xorg || true
-nvidia-smi -pm 0
+# nvidia-smi -pm 0
 
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
@@ -19,5 +19,5 @@ apt-get purge -qqy "cuda-drivers*" "*nvidia*-${CUDA_DRIVER_VERSION}"
 apt-get install -qqy "cuda-drivers"
 
 modprobe -r nvidia_drm nvidia_uvm nvidia_modeset nvidia
-nvidia-smi -pm 1
+# nvidia-smi -pm 1
 nvidia-smi
