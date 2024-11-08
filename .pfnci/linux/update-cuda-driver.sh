884b49b5dc1aaa6807b2fd3e50fa3092056b160a
Daniel Rodriguez <daniel@danielfrg.com>
2024-11-08 14:53:17 -0600

    sudo

--- .pfnci/linux/update-cuda-driver.sh
+++ .pfnci/linux/update-cuda-driver.sh
@@ -10,14 +10,16 @@ else
     echo "No cuda-drivers currently installed"
 fi
 
+CUDA_DRIVER_VERSION=565
+
 killall Xorg || true
-# nvidia-smi -pm 0
+nvidia-smi -pm 0
 
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
 apt-get purge -qqy "cuda-drivers*" "*nvidia*-${CUDA_DRIVER_VERSION}"
 apt-get install -qqy "cuda-drivers"
 
-modprobe -r nvidia_drm nvidia_uvm nvidia_modeset nvidia
-# nvidia-smi -pm 1
+sudo modprobe -r nvidia_drm nvidia_uvm nvidia_modeset nvidia
+nvidia-smi -pm 1
 nvidia-smi
